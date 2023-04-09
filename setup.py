35a873f9c77f84901650b146026ed3a277ee6158
Borre Pol Meeuwisse <me@borrepol.nl>
2023-04-10 00:33:00 +0200

    Fixed init problem

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='DotDict',
-    version='0.0.1',
+    version='0.0.2',
     author='Borre Pol Meeuwisse',
     author_email='me@borrepol.nl',
     description='Dictionary that enables referencing of items using `<dict>.<key>`.',
